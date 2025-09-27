166342fa9e8f2aacc1ec5ce33c9c68418b15dfff
Mo Amer <moamrr@gmail.com>
2025-09-27 12:03:20 +0100

    updated vercel configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,19 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		paths: {
 			base: ''
-		},
-		prerender: {
-			handleHttpError: 'warn',
-			handleUnseenRoutes: 'warn'
 		}
 	}
 };
